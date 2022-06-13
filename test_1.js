0b6aa614abd14b902e7ed0cc9fe05e2ee356aad6
Kateintheforest <71213674+Kateintheforest@users.noreply.github.com>
2022-06-13 22:48:03 +0500

    Update test_1.js
    селектор для желтой кнопки

--- test_1.js
+++ test_1.js
@@ -6,12 +6,17 @@ describe("My first test", () => {
   it("найти в поиске", () => {
     cy.get("input[title='Поиск']").type("Byndyusoft").type("{enter}");
   });
+
   it("первая ссылка", () => {
     cy.get("div#search a").first().click();
   });
 
   it("нажать кнопку", () => {
-    cy.contains("Заказать презентацию").click();
+    cy.get('[class="know-more"]')
+      //.get('[class*="btn--info"]')    можно вместо привязки к секции привязать к классу самой кнопки
+      .contains("Заказать презентацию")
+      //.should("have.css", "background-color", "rgb(255, 214, 87)")  привязка именно к цвету кнопки
+      .click();
   });
 
   it("проверить почту", () => {
